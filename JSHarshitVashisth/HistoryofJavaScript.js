d9b43e1c865b73d6beaf531e56af1856712a6014
AsifAliDeveloper <letmeetasif@gmail.com>
2025-07-05 18:07:19 +0500

    modified file

--- JSHarshitVashisth/HistoryofJavaScript.js
+++ JSHarshitVashisth/HistoryofJavaScript.js
@@ -1,17 +1,17 @@
-//   🔹  In 1995 => Brendan Eich ==> Make JavaScript in 10 Days =>  A popular web browser Netscape Navigator
-//   🔹  Name of JavaScript    ==>  Mocha - LiveScript - JavaScript 
+//?   🔹  In 1995 => Brendan Eich ==> Make JavaScript in 10 Days =>  A popular web browser Netscape Navigator
+//?   🔹  Name of JavaScript    ==>  Mocha - LiveScript - JavaScript 
 
-//   🔹  Internet Explorer ==> Make JScript
+//?   🔹  Internet Explorer ==> Make JScript
 
-//   🔹  In 1996     Netscape Navigator     ||         Internet Explorer
-//   🔹  In 1996     JavaScript             ||          JScript 
+//?   🔹  In 1996     Netscape Navigator     ||         Internet Explorer
+//?   🔹  In 1996     JavaScript             ||          JScript 
 
-//   🔹 ECMA  ==> ECMA stands for European Computer Manufacturers Association. However, in web development and JavaScript context, ECMA usually refers to ECMAScript, the standard that defines the scripting language specification for languages like JavaScript, JScript, and ActionScript.
+//*   🔹 ECMA  ==> ECMA stands for European Computer Manufacturers Association. However, in web development and JavaScript context, ECMA usually refers to ECMAScript, the standard that defines the scripting language specification for languages like JavaScript, JScript, and ActionScript.
 
-//   🔹  What is ECMAScript (ES)?
+//?   🔹  What is ECMAScript (ES)?
 // ECMAScript is a scripting-language specification standardized by ECMA International in the ECMA-262 document. JavaScript is the most well-known implementation of ECMAScript.
 
-//   🔹  History and Versions of ECMAScript:
+//?   🔹  History and Versions of ECMAScript:
 // Version	Release Year	Key Features
 // ES1	1997	First edition
 // ES2	1998	Editorial changes
@@ -27,12 +27,12 @@
 // ES13 (2022)	2022	Top-level await, class fields
 // ES14 (2023)	2023	Array.findLast(), Array.findLastIndex()
 
-//   🔹 ECMA vs JavaScript
+//?   🔹 ECMA vs JavaScript
 // ECMA defines the language specification.
 
 // JavaScript is the actual implementation used in browsers and environments like Node.js.
 
-//  🔹  TC 39 ==> Technical Community 
+//?  🔹  TC 39 ==> Technical Community 
 
 // JavaScript Backward Compatible
 // JavaScript is't Forward compatible
